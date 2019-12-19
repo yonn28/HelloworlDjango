75d8f2c0a4fc81b877dba45de8badf31dae98cd9
yonn28 <ycnovac@unal.edu.co>
2019-12-19 17:31:53 -0500

    working with bring the id and display into form to edit

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.mascota.views import index, mascota_view
+from apps.mascota.views import index, mascota_view,mascota_list,mascota_edit
 
 urlpatterns = [
     path('', index, name='index'),
     path('nuevo', mascota_view, name='mascota_crear'),
+    path('listar', mascota_list, name='mascota_listar'),
+    path('editar/<int:pk>/', mascota_edit, name='mascota_editar'),
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from apps.mascota.forms import MascotaForm
+from django.db import models
+from .models import Mascota
 
 # Create your views here.
 
@@ -14,7 +16,23 @@ def mascota_view(request):
         form = MascotaForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('index')
+        return redirect('index') ## no se puede colocar el subindice porque no lo toma!!!
     else:
         form=MascotaForm()       
     return render(request, 'mascota/mascota_form.html',{'form':form})
+
+def mascota_list(request):
+    mascota=Mascota.objects.all()
+    contexto = {'mascotas':mascota}
+    return render(request, 'mascota/mascota_list.html',contexto)
+
+def mascota_edit(request, pk):
+    mascota=Mascota.objects.get(id=pk)
+    if request.method == 'GET':
+        form=MascotaForm(instance=mascota)
+    else:
+        form=MascotaForm(request.POST,instance=mascota)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota_listar')
+    return render(request,'mascota/mascota_form.html',{'form':form})
