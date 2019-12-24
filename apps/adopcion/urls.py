68e6d2ae95f58e3afba883e91de33cd9c6aca842
yonn28 <ycnovac@unal.edu.co>
2019-12-24 10:27:22 -0500

    With working adding a pet

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from apps.adopcion.views import index, SolicitudList,SolicitudCreate,SolicitudUpdate,SolicitudDelete
 
-appname='adopcion'
+app_name='adopcion'
 
 urlpatterns = [
-    path('', views.index, name='adopcion'),
+    path('', views.index, name='index'),
+    path('solictud/listar', SolicitudList.as_view(), name='solicitud_listar'),
+    path('solictud/nueva', SolicitudCreate.as_view(), name='solicitud_crear'),
+    path('solictud/editar/<int:pk>', SolicitudUpdate.as_view(), name='solicitud_editar'),
+    path('solicitud/eliminar/<int:pk>',SolicitudDelete.as_view(),name='solicitud_eliminar'),
 ]
